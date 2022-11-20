fc684138d69f62d7483bfd7c5b218aae9e387968
Businge Bisanga <jimmybusinge9@gmail.com>
2022-11-20 14:56:34 +0300

    ui changes

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "../../Button/Button";
+import Footer from "../../Footer/Footer";
+import Navbar from "../../Navbar/Navbar";
+import HowToUse from "../How To Use/HowToUse";
+import WhyUs from "../Why Us/WhyUs";
+import classes from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className={classes.home}>
+        <Navbar background={classes.background} />
+        <div className={classes.home_content}>
+          <h1>Decision Support Tool for Inclusive Sanitation Intervention</h1>
+          <p>
+            Spatially demarcate area at settlement level and recommend on the
+            suitable sanitation intervention options to decision makers.support
+            in decision making for range of sanitation intervention in cityscape
+          </p>
+          <Button />
+        </div>
+      </div>
+
+      <WhyUs />
+      <HowToUse />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
